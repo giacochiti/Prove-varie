670d68004df090964ed8cb163c18d71388caf895
giacochiti <giaco.chiti@gmail.com>
2025-02-01 14:12:01 +0100

    Update update_somma.py

--- update_somma.py
+++ update_somma.py
@@ -1,6 +1,6 @@
 import os
 
-def legi_numero(file_path):
+def leggi_numero(file_path):
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
             contenuto = file.read().strip()
